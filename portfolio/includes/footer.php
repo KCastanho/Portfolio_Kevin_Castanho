dca4ee0beb9efdfda12a5c3e26c6599ff3a53208
KCastanho <kevincastanho.pro@gmail.com>
2025-02-13 09:36:24 +0100

    sup dossier

--- portfolio/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<script>
-        document.querySelector('.menu-toggle').addEventListener('click', function() {
-            document.querySelector('.liens').classList.toggle('active');
-        });
-    </script>
-</body>
-</html>
